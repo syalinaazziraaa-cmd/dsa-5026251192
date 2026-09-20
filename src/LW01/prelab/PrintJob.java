ad0059ae6e01cf5c5da62fc199ce98873f47beb4
Syalina Azzira Rifdah <syalinaazziraaa@gmail.com>
2026-09-20 21:00:49 +0700

    Labwork 01 - Prelab

---
+++ src/LW01/prelab/PrintJob.java
@@ -0,0 +1,34 @@
+package LW01.prelab;
+
+public abstract class PrintJob implements Chargeable{
+    private String id;
+    private int pages;
+
+    protected PrintJob(String id, int pages){
+        this.id = id;
+        this.pages = pages;
+    }
+
+    public String getid(){
+        return id;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    @Override 
+    public abstract int calculateCharge();
+
+    public int calculateCharge(int copies){
+        return copies * calculateCharge();
+    }
+
+    public String label(){
+        return "Print";
+    }
+
+    public String summary(){
+        return id + " | " + label() + " | " + calculateCharge();
+    }  
+}
